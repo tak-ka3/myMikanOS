1ab1c98c29dfee90bc51ead7c6b1397382b18560
tak-ka3 <hi.ta9.23.333@gmail.com>
2022-02-28 17:42:15 +0900

    fix tabsize

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -26,11 +26,11 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
 
   map->map_size = map->buffer_size;
   return gBS->GetMemoryMap(
-    &map->map_size,
-    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
-    &map->map_key,
-    &map->descriptor_size,
-    &map->descriptor_version);
+	&map->map_size,
+	(EFI_MEMORY_DESCRIPTOR*)map->buffer,
+	&map->map_key,
+	&map->descriptor_size,
+	&map->descriptor_version);
 }
 // #@@range_end(get_memory_map)
 
