4a65adb55f8aa64b50d1d55fceee5def94c049da
tak-ka3 <hi.ta9.23.333@gmail.com>
2022-02-28 17:38:55 +0900

    first commit

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,10 +1,63 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/PrintLib.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Protocol/DiskIo2.h>
+#include <Protocol/BlockIo.h>
+
+// #@@range_begin(struct_memory_map)
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+// #@@range_end(struct_memory_map)
+
+// #@@range_begin(get_memory_map)
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+    &map->map_size,
+    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+    &map->map_key,
+    &map->descriptor_size,
+    &map->descriptor_version);
+}
+// #@@range_end(get_memory_map)
+
+
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
+    EFI_SYSTEM_TABLE* system_table) {
   Print(L"Hello, Mikan World!\n");
+
+  // #@@range_begin(main)
+  CHAR8 memmap_buf[4096 * 4];
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  GetMemoryMap(&memmap)
+
+  EFI_FILE_PROTOCOL* root_dir;
+  OpenRootDir(image_handle, &root_dir);
+
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(
+    root_dir, &memmap_file, L"\\memmap",
+    EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+
+  Print(L"All done/n");
   while (1);
   return EFI_SUCCESS;
 }
