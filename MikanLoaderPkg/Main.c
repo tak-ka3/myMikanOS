6aab8856e7fc882b32a2868c7704a8bb75dae354
tak-ka3 <hi.ta9.23.333@gmail.com>
2022-02-28 17:40:34 +0900

    fix spaces

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,11 +1,11 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/PrintLib.h>
-#include <Protocol/LoadedImage.h>
-#include <Protocol/SimpleFileSystem.h>
-#include <Protocol/DiskIo2.h>
-#include <Protocol/BlockIo.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
 
 // #@@range_begin(struct_memory_map)
 struct MemoryMap {
